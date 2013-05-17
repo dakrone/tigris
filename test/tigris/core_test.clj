1f7fa6d1af4b38f6dcb14063be98b566f73736ca
Lee Hinman <lee@writequit.org>
2013-05-17 10:13:21 -0600

    start of tests

--- test/tigris/core_test.clj
+++ test/tigris/core_test.clj
@@ -1,7 +1,25 @@
 (ns tigris.core-test
   (:require [clojure.test :refer :all]
-            [tigris.core :refer :all]))
+            [cheshire.core :refer :all]
+            [tigris.core :refer :all])
+  (:import (java.io ByteArrayInputStream)))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn stream-for
+  [s]
+  {:pre [(string? s)]}
+  (str-escaping-input-stream (ByteArrayInputStream. (.getBytes s))))
+
+(defn json-for
+  [s]
+  {:pre [(string? s)]}
+  (encode {:thing s}))
+
+(defn json-map-for-str
+  "helper for generating a valid json map for a string"
+  [s]
+  (str "{\"thing\":\"" s "\"}"))
+
+(deftest t-basic
+  (let [s1 "this is \"some\" text with quotes."]
+    (is (= (json-map-for-str (slurp (stream-for s1)))
+           (json-for s1)))))
