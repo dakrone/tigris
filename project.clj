343836f1d0d9dd3d19717f8931fb5a6c217a0bd8
Lee Hinman <lee@writequit.org>
2013-05-17 12:24:54 -0600

    bump to 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tigris "0.1.0"
+(defproject tigris "0.1.1-SNAPSHOT"
   :description "Stream-to-stream JSON string encoding"
   :url "https://github.com/dakrone/tigris"
   :license {:name "Eclipse Public License"
