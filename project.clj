5bc1451844de267cb8e799456ad96c6cb6e67d13
Andrea Richiardi <a.richiardi.work@gmail.com>
2016-06-09 17:20:58 -0700

    Change org.clojure/clojure to "provided"
    This way it won't be imported as transitive dependency anymore.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/dakrone/tigris"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1" :scope "provided"]
   :profiles {:dev {:dependencies [[cheshire "5.1.1"]]}}
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"])
