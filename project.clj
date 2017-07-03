f96b199048a7f5e1efcaccf2dbc927ade919bbef
Dave Yarwood <dave.yarwood@gmail.com>
2017-07-03 08:35:49 -0400

    remove explicit clj dep; add dev dep on clj 1.8.0

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/dakrone/tigris"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[cheshire "5.1.1"]]}}
+  :dependencies []
+  :profiles {:dev {:dependencies [[cheshire "5.1.1"]
+                                  [org.clojure/clojure "1.8.0"]]}}
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"])
